976dee2f2fd4b59978f87255a4a6cfd675007064
Carlos Suarez <me@carlossz.co>
2018-07-02 11:05:43 -0500

    :lock: http to https

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -15,7 +15,7 @@ export class SearchForm extends Component {
     e.preventDefault()
     const {inputMovie} = this.state
     
-    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
       .then(res => res.json())
       .then(results => {
         const { Search = [] } = results
